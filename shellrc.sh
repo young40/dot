8cdf8125276ff9eadf299e381707275b40e464a6
Peter Young <young40@gmail.com>
2017-06-17 23:10:32 +0800

    翻墙支持

--- shellrc.sh
+++ shellrc.sh
@@ -1,6 +1,10 @@
 # 使用brew版本curl
 export PATH="/usr/local/opt/curl/bin:$PATH"
 
+# for GFW
+alias fq="proxychains4 zsh"
+alias ip="echo ip.gs:; curl ip.gs; echo ipip.net:; curl myip.ipip.net"
+
 if [ -f shell-private-key.sh ]
 then
    source shell-private-key.sh
