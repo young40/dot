3b862e9a904b6564e8db6d9571a49af67aad93fd
Peter Young <young40@gmail.com>
2017-06-22 14:22:19 +0800

    更新Xianlai命令

--- shell-Xianlai.sh
+++ shell-Xianlai.sh
@@ -1,17 +1,25 @@
 openSimulator()
 {
-    app="`echo $1`runtime/mac/mahjonghn-desktop$2.app/Contents/MacOS/mahjonghn-desktop"
+    dir=~/Work/Sichuan/sichuan_debug/
+    app="`echo $dir`runtime/mac/mahjonghn-desktop$1.app/Contents/MacOS/mahjonghn-desktop"
 
-    rm -rf "`echo $1`src_et"
-    $app -console disable -workdir $1
+    rm -rf "${dir}src_et"
+    $app -console disable -workdir $dir $2 $3
 }
 
-alias v="openSimulator ~/Work/Sichuan/sichuan_debug/"
-alias v2="openSimulator ~/Work/Sichuan/sichuan_debug/ 2"
-alias v3="openSimulator ~/Work/Sichuan/sichuan_debug/ 3"
-alias v4="openSimulator ~/Work/Sichuan/sichuan_debug/ 4"
+openSimulatorForUpdateDir()
+{
+    rm -rf ~/Work/Sichuan/for_sichuan_debug_update/src_et/
+    openSimulator $1 -workdir ~/Work/Sichuan/for_sichuan_debug_update/
+}
 
-alias vf="openSimulator ~/Work/Sichuan/sichuan_debug/"
-alias vf2="openSimulator ~/Work/Sichuan/sichuan_debug/ 2"
-alias vf3="openSimulator ~/Work/Sichuan/sichuan_debug/ 3"
-alias vf4="openSimulator ~/Work/Sichuan/sichuan_debug/ 4"
+alias  v="openSimulator 1"
+alias v2="openSimulator 2"
+alias v3="openSimulator 3"
+alias v4="openSimulator 4"
+
+
+alias  vf="openSimulatorForUpdateDir 1"
+alias vf2="openSimulatorForUpdateDir 2"
+alias vf3="openSimulatorForUpdateDir 3"
+alias vf4="openSimulatorForUpdateDir 4"
