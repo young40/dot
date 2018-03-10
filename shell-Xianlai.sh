1ddd5bf7b4e5f9fddbcba29324f5e27a47db5eb3
Peter Young <young40@gmail.com>
2018-03-10 11:16:52 +0800

    xl云南项目

--- shell-Xianlai.sh
+++ shell-Xianlai.sh
@@ -13,6 +13,12 @@ openSimulatorForUpdateDir()
     openSimulator $1 -workdir /Users/young40/Work/sichuan/for_sichuan_debug_update
 }
 
+openSimulatorForYunnan()
+{
+    rm -rf /Users/young40/Work/yunnan/src_et/
+    openSimulator $1 -workdir /Users/young40/Work/yunnan
+}
+
 alias  v="openSimulator 1"
 alias v2="openSimulator 2"
 alias v3="openSimulator 3"
@@ -23,6 +29,8 @@ alias  vf="openSimulatorForUpdateDir 1"
 alias vf2="openSimulatorForUpdateDir 2"
 alias vf3="openSimulatorForUpdateDir 3"
 alias vf4="openSimulatorForUpdateDir 4"
+
+alias y="openSimulatorForYunnan 1"
 
 xianlaigit()
 {
