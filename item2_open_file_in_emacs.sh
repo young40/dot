113ca11fad22519f8154c64550f415969b4212cc
Peter Young <young40@gmail.com>
2018-03-13 23:52:40 +0800

    item2_open_file_in_emacs.sh

---
+++ item2_open_file_in_emacs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exec &> ~/logfile.txt
+
+# In iTerm2 Session preferences <Advanced> pane setup Semantic History to:
+#
+# [Run coprocess...]
+# ~/dot/item2_open_file_in_emacs.sh +\2 \1
+
+if [[ $1 == "+" ]]; then
+    shift
+fi
+
+/usr/local/bin/emacsclient -q -n -a "" $1 $2 || open -a /Applications/Emacs.app/Contents/MacOS/Emacs &
+
