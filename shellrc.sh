04f1150303a4a1604aa1303dced069ddbc46cd26
Peter Young <young40@gmail.com>
2018-11-20 22:26:07 +0800

    react-native 命令缩写

--- shellrc.sh
+++ shellrc.sh
@@ -30,6 +30,9 @@ alias o="cd ~/org/ && git add . && git commit -m 'quick update' && git push && e
 # for Lua
 alias lua="lua -e 'package.path = package.path .. \";/Users/$USER/dot/?.lua\" require(\"lua-dump\")'"
 
+# for React Native
+alias rn=react-native
+
 # for NeoVim
 alias vim=nvim
 
