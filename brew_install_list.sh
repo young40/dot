d41414cb61579bc7778ce6bedff4badfdf33585b
Peter Young <young40@gmail.com>
2019-02-04 10:19:51 +0800

    brew install

--- brew_install_list.sh
+++ brew_install_list.sh
@@ -1,2 +1,39 @@
 
 brew install hexyl
+brew install nmap
+brew install dex2jar
+brew install git-lfs
+
+# brew install go
+# brew install tgz
+# brew install hugo
+# brew install neovim
+# brew install imagemagick
+# brew install telnet
+# brew install ftp
+# brew install ipa
+# brew install apksigner
+# brew uninstall pure-ftpd
+# brew uninstall mp3info
+# brew install ffmepg
+# brew install ffmpeg
+# brew install apktool
+# brew install luarocks
+# brew install ndoe
+# brew install node
+# brew install imgcat
+# brew install imgcat
+# brew install mp3info
+# brew cask install id3-editor
+# brew cask install mp3tag
+# brew install lame
+# brew install scrcpy
+# brew install tig
+# brew install stat
+# brew cask install pusher
+# brew install warchman
+# brew install watchman
+# brew install yarn
+# brew install luyten
+# brew install git-lfs
+# brew install dex2jar
