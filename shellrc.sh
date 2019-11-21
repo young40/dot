3e9276f93fba9844386dca198f024878ceb782d2
Peter Young <young40@gmail.com>
2019-11-21 21:43:39 +0800

    博客命令

--- shellrc.sh
+++ shellrc.sh
@@ -44,6 +44,7 @@ ip()
 # for Org files
 alias o="cd ~/Work/Org/ && git add . && git commit -m 'quick update' && git push && echo 'Org pushed'; cd -"
 alias oi="cd ~/Work/Org/ && git diff && cd -"
+alias ob="cd ~/Work/Young40Blog && git pull && git add . && git commit -m 'quick update' && git push && echo 'Blog content stored.'; cd -"
 
 # for Lua
 alias lua="lua -e 'package.path = package.path .. \";/Users/$USER/dot/?.lua\" require(\"lua-dump\")'"
